7f444294c580afc7e52295056df3b7a00fa85ebe
Bin Yong <yongb@usi.ch>
2024-05-20 20:11:06 +0000

    Update oneplus-download.py

--- Example/oneplus-download.py
+++ Example/oneplus-download.py
@@ -1,9 +1,40 @@
 #!/bin/env python3
+import inspect
+import re
+import types
 import xml.etree.ElementTree as ET
+from typing import cast
+
+def __FUNCTION__():
+  return cast(types.FrameType, inspect.currentframe()).f_back.f_code.co_name
+
+def handle_SAHARA(node):
+  handlers = {
+  }
+  for child in node:
+    if child.tag in handlers:
+      handlers[child.tag]()
+    else:
+      print(f"{__FUNCTION__()}: Unsupported tag '{child.tag}'")
+  
+
+def handle_root(node):
+  handlers = {
+    re.compile("BasicInfo"): lambda child: print(child.tag, child.attrib),
+    re.compile("SAHARA"): handle_SAHARA,
+    re.compile("MEMORY_SIZE"): lambda x: None
+  }
+  for child in node:
+    supported = False
+    for tag_regex, handler in handlers.items():    
+      if tag_regex.fullmatch(child.tag):
+        handler(child)
+        supported = True
+    if not supported:
+      print(f"{__FUNCTION__()}: Unsupported tag '{child.tag}'")
+
+
 tree = ET.parse('settings.xml')
 root = tree.getroot()
 assert root.tag == 'Setting'
-print(root.tag)
-for child in root:
-  if child.tag == "BasicInfo":
-    print(child.tag, child.attrib)
+handle_root(root)
