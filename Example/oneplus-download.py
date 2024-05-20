6feb16d1547d17796c3a182af30e056903e32082
Bin Yong <yongb@usi.ch>
2024-05-20 20:38:12 +0000

    Update oneplus-download.py

--- Example/oneplus-download.py
+++ Example/oneplus-download.py
@@ -8,21 +8,41 @@ from typing import cast
 def __FUNCTION__():
   return cast(types.FrameType, inspect.currentframe()).f_back.f_code.co_name
 
+info_ = {}
+
+def save_info(key, value):
+  global info_
+  info_[key] = value
+  
+
 def handle_SAHARA(node):
   handlers = {
+    "File": lambda x: save_info("loader", x.attrib['Path'])
   }
   for child in node:
     if child.tag in handlers:
-      handlers[child.tag]()
+      handlers[child.tag](child)
     else:
       print(f"{__FUNCTION__()}: Unsupported tag '{child.tag}'")
   
 
+def handle_Program(node):
+  handlers = {
+    "program": lambda x: print(x.attrib)
+  }
+  for child in node:
+    if child.tag in handlers:
+      handlers[child.tag](child)
+    else:
+      print(f"{__FUNCTION__()}: Unsupported tag '{child.tag}'")
+  
 def handle_root(node):
   handlers = {
     re.compile("BasicInfo"): lambda child: print(child.tag, child.attrib),
     re.compile("SAHARA"): handle_SAHARA,
-    re.compile("MEMORY_SIZE"): lambda x: None
+    re.compile("MEMORY_SIZE"): lambda x: None,
+    re.compile("UFS_PROVISION"): lambda x: print(f"Help needed on understanding {x.tag}"),
+    re.compile("Program\d+"): handle_Program,
   }
   for child in node:
     supported = False
